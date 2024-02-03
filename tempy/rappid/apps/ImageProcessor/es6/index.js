a6813f51262f150f553be2f6c740926511ddc434
vilas.c.p <vilas.c.p@vilascps-MacBook-Air.local>
2024-02-03 21:26:07 +0530

    Hello

--- tempy/rappid/apps/ImageProcessor/es6/index.js
+++
@@ -1,7 +0,0 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import '@clientio/rappid/rappid.css';
-import './styles.scss';
-import { App } from './src/app';
-
-export const app = new App();
